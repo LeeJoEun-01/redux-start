4551fe4ca863a7f775f911bbc827661ffe2cd4e9
LeeJoEun-01 <joeun1005@naver.com>
2022-04-13 13:29:24 +0900

    Create Project(2)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import useReduxState from './hooks/useReduxState';
-import useReduxDispatch from './hooks/useReduxDispatch';
 import TodoListContainer from './containers/TodoListContainer';
 import TodoFormContainer from './containers/TodoFormContainer';
+import UserListContainer from './containers/UserListContainer';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <UserListContainer />
         <TodoListContainer />
         <TodoFormContainer />
         {/* {JSON.stringify(state)} */}
---
+++ src/containers/UserListContainer.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import UserList from "../components/UserList";
+import { getUsersFail, getUsersStart, getUsersSuccess } from "../redux/actions";
+
+export default function UserListContainer() {
+  const users = useSelector((state) => state.users.data);
+  const dispatch = useDispatch();
+
+  // const start = useCallback(() => {
+  //   dispatch(getUsersStart());
+  // }, [dispatch]);
+
+  // const success = useCallback((data) => {
+  //   dispatch(getUsersSuccess(data));
+  // }, [dispatch]);
+
+  // const fail = useCallback((error) => {
+  //   dispatch(getUsersFail(error));
+  // }, [dispatch]);
+
+  const getUsers = useCallback(async function getUsers() {
+    try {
+      dispatch(getUsersStart());
+      const res = await axios.get("https://api.github.com/users");
+      dispatch(getUsersSuccess(res.data));
+    } catch (error) {
+      dispatch(getUsersFail(error));
+    }
+  }, [dispatch]);
+
+  return <UserList users={users} getUsers={getUsers} />
+}
